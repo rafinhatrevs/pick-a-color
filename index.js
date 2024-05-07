3d46f735d37ae72b76a971b753b8d6e035ad3e7a
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-05-07 18:29:45 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
-const btn = document.getElementById('btn') 
-const color = document.querySelector('.color')
+const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
-//No click
-btn.addEventListener('click', function() {
-    const randomColor = getRandomColor()
-    document.body.style.background = colors[randomColor] //Aplicando a cor ao background
-    color.textContent = colors[randomColor]
-})
- 
-//Gerar os numeros randomicos
+// no click
+btn.addEventListener('click', function () {
+    const random_color = getRandomColor();
+    document.body.style.background = colors[random_color]; // aplicando a cor ao background
+    color.textContent = colors[random_color];
+});
+
+// gerar os numeros randomicos
 function getRandomColor() {
-    return Math.floor(Math.random() * colors.length)
-
+    return Math.floor(Math.random() * colors.length);
 }
