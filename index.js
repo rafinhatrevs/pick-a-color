7e2205dcdb71f0acedfd656ea9dc6630489e205a
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-05-28 19:00:02 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-// no click
 btn.addEventListener('click', function () {
     const random_color = getRandomColor();
-    document.body.style.background = colors[random_color]; // aplicando a cor ao background
+    document.body.style.background = colors[random_color];
     color.textContent = colors[random_color];
 });
 
-// gerar os numeros randomicos
 function getRandomColor() {
     return Math.floor(Math.random() * colors.length);
 }
